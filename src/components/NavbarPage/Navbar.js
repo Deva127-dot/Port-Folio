ee7bc87be056157e503b4cfc318f60a15b95a17c
Devendra <devendraparsharam127@gmail.com>
2023-07-18 21:20:36 +0530

    my port folio

--- src/components/NavbarPage/Navbar.js
+++ src/components/NavbarPage/Navbar.js
@@ -7,32 +7,33 @@ function Navbar() {
   return (
     <div className={styles.main}>
       <div className={styles.sub_main}>
-        <Link to="/" className={styles
-        .link} >
+        <Link to="/" className={styles.link}>
           <p className={styles.para}>HOME</p>
         </Link>
-        <Link to="/work" className={styles
-        .link}>
+        <Link to="/work" className={styles.link}>
           {" "}
           <p className={styles.para}>WORK</p>
         </Link>
-        <Link to="/tools" className={styles
-        .link}>
+        <Link to="/tools" className={styles.link}>
           {" "}
           <p className={styles.para}>TOOLS & SKILLS</p>
         </Link>
-        <Link to="/contact" className={styles
-        .link}>
+        <Link to="/contact" className={styles.link}>
           {" "}
           <p className={styles.para}>CONTACT</p>
         </Link>
-        <a href="https://drive.google.com/file/d/18RFT-2Vd2YngH7_QZTJTUYGfBW6VBF9E/view?usp=sharing" target="_blank" className={styles
-        .link}><button className={styles.btn}>
-          DOWNLOAD RESUME{" "}
-          <a className={styles.dwnldIcon}>
-            <ArrowDownwardIcon />
-          </a>
-        </button></a>
+        <a
+          href="https://drive.google.com/file/d/18RFT-2Vd2YngH7_QZTJTUYGfBW6VBF9E/view?usp=sharing"
+          target="_blank"
+          className={styles.link}
+        >
+          <button className={styles.btn}>
+            DOWNLOAD RESUME{" "}
+            <a className={styles.dwnldIcon}>
+              <ArrowDownwardIcon />
+            </a>
+          </button>
+        </a>
       </div>
     </div>
   );
